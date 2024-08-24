39594c832692d36a84610690430971f223908c2b
Darlin Daniel Arias Méndez <darlin.informatica@gmail.com>
2024-08-24 15:49:28 -0400

    UI updated.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,27 @@
+import { ChangeEvent } from 'react';
+import { useLang } from '../../hooks/lang/useLang';
+import { LangIcon } from '../Icons/Icons';
+import { Lang } from '../../types/langTypes';
+
 export default function Header() {
+    const { lang, langText, changeLang } = useLang();
+
+    const handleChange = (event: ChangeEvent<HTMLSelectElement>) => {
+        const newLang = event.target.value;
+        if (changeLang) changeLang({ newLang: newLang as Lang });
+    }
+
     return (
-        <header className='w-full h-20 grid place-content-center border-b border-zinc-500'>
+        <header className='w-full h-20 flex justify-between items-center border-b border-zinc-500 px-14'>
             <div>
-                <h1 className='text-lg' >Lista de tareas.</h1>
+                <h1 className='text-lg' >{ langText?.header?.title }</h1>
+            </div>
+            <div className='flex justify-center items-center gap-1' >
+                <LangIcon />
+                <select name="lang" onChange={handleChange} value={ lang?.currentLang } >
+                    <option value="es">Español</option>
+                    <option value="en">English</option>
+                </select>
             </div>
         </header>
     );
