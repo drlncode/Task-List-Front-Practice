267416b055a32f802f389df256107742c9c0f5bd
Darlin Daniel Arias Méndez <darlin.informatica@gmail.com>
2024-08-24 14:56:06 -0400

    Finishing adding persistence for the Lang Context.

--- src/contexts/lang/langProvider.tsx
+++ src/contexts/lang/langProvider.tsx
@@ -1,8 +1,13 @@
+import { useEffect } from 'react';
 import { LangContext } from './langContext';
 import { useContextState } from '../../hooks/lang/useContextState';
 
 export function LangProvider({ children }: { children: JSX.Element }) {
     const { lang, changeLang  } = useContextState();
+
+    useEffect(() => {
+        window.localStorage.setItem('lang', JSON.stringify(lang));
+    }, [lang]);
 
     return (
         <LangContext.Provider value={{
